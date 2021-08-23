559a67e047a6dfae5c0ed8993d1fc42c73df5e31
paolopast95 <paolo.pastore295@gmail.com>
2021-08-23 15:53:27 +0200

    gru and rnn added

---
+++ src/models/gru.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Bidirectional, Dense, GRU, Embedding
+from tensorflow.keras import Model
+
+class CustomizedGRU(Model):
+    def __init__(self, num_classes=1, num_hidden_layers=2, num_recurrent_units=[256,512], num_dense_layers=2, num_dense_neurons=[64], is_bidirectional=False, vocab_size=5000, pretrained_embeddings=None):
+        super(CustomizedGRU, self).__init__()
+        self.grus = []
+        self.denses = []
+        if pretrained_embeddings:
+            self.embedding = tf.keras.layers.Embedding(vocab_size, pretrained_embeddings.shape(0),
+                                                       embeddings_initializer=tf.keras.initializers.Constant(pretrained_embeddings),
+                                                       trainable=False)
+        else:
+            self.embedding = Embedding(vocab_size,300)
+        if is_bidirectional:
+            for i in range(num_hidden_layers - 1):
+                self.grus.append(Bidirectional(GRU(num_recurrent_units[i], activation="relu", return_sequences=True)))
+            self.grus.append(Bidirectional(GRU(num_recurrent_units[num_hidden_layers - 1], activation="relu", return_sequences=False)))
+        else:
+            for i in range(num_hidden_layers-1):
+                self.grus.append(GRU(num_recurrent_units[i], activation="relu", return_sequences=True))
+            self.grus.append(GRU(num_recurrent_units[num_hidden_layers-1], activation="relu", return_sequences=False))
+        for i in range(num_dense_layers):
+            self.denses.append(tf.keras.layers.Dense(num_dense_neurons[i], activation="relu"))
+        self.classification_layer = tf.keras.layers.Dense(num_classes, activation="sigmoid")
+
+    def call(self, inputs):
+        x = self.embedding(inputs)
+        for layer in self.grus:
+            x = layer(x)
+        for layer in self.denses:
+            x = layer(x)
+        return self.classification_layer(x)
--- src/models/lstm.py
+++ src/models/lstm.py
@@ -1,34 +1,41 @@
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import MultinomialNB
-import itertools
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.metrics import accuracy_score
 import pandas as pd
 from src.preprocessing.stemming import Stemmer
 from src.preprocessing.tokenization import Tokenizer
 from src.preprocessing.stopword_removal import StopwordRemoval
-from src.preprocessing.sentence_embedding import Vectorizer
-from tqdm import tqdm
+from tensorflow.keras.layers import Bidirectional, Dense, LSTM, Embedding
+from tensorflow.keras import Model
 
-class CustomizedLSTM(tf.keras.Model):
-    def __init__(self, num_classes=2, num_hidden_layers=2, num_recurrent_units=[256,512], num_dense_layers=2, num_dense_neurons=[64], is_bidirectional=False, vocab_size=5000):
+class CustomizedLSTM(Model):
+    def __init__(self, num_classes=1, num_hidden_layers=2, num_recurrent_units=[256,512], num_dense_layers=2, num_dense_neurons=[64], is_bidirectional=False, vocab_size=5000, pretrained_embeddings=None):
         super(CustomizedLSTM, self).__init__()
         self.lstms = []
-        self.embedding = tf.keras.layers.Embedding(vocab_size,300)
-        for i in range(num_hidden_layers):
-            self.lstms.append(tf.keras.layers.LSTM(num_recurrent_units[i], activation="relu", return_sequences=True))
-        self.lstms.append(tf.keras.layers.LSTM(num_recurrent_units[0], activation="relu", return_sequences=False))
-        self.first_dense = tf.keras.layers.Dense(num_dense_neurons[0], activation="relu")
-        self.classification_layer = tf.keras.layers.Dense(1, activation="sigmoid")
+        self.denses = []
+        if pretrained_embeddings:
+            self.embedding = tf.keras.layers.Embedding(vocab_size, pretrained_embeddings.shape(0),
+                                                       embeddings_initializer=tf.keras.initializers.Constant(pretrained_embeddings),
+                                                       trainable=False)
+        else:
+            self.embedding = Embedding(vocab_size,300)
+        if is_bidirectional:
+            for i in range(num_hidden_layers - 1):
+                self.lstms.append(Bidirectional(LSTM(num_recurrent_units[i], activation="relu", return_sequences=True)))
+            self.lstms.append(Bidirectional(LSTM(num_recurrent_units[num_hidden_layers - 1], activation="relu", return_sequences=False)))
+        else:
+            for i in range(num_hidden_layers-1):
+                self.lstms.append(LSTM(num_recurrent_units[i], activation="relu", return_sequences=True))
+            self.lstms.append(LSTM(num_recurrent_units[num_hidden_layers-1], activation="relu", return_sequences=False))
+        for i in range(num_dense_layers):
+            self.denses.append(tf.keras.layers.Dense(num_dense_neurons[i], activation="relu"))
+        self.classification_layer = tf.keras.layers.Dense(num_classes, activation="sigmoid")
 
     def call(self, inputs):
         x = self.embedding(inputs)
         for layer in self.lstms:
             x = layer(x)
-        x = self.first_dense(x)
+        for layer in self.denses:
+            x = layer(x)
         return self.classification_layer(x)
 
 
@@ -52,7 +59,7 @@ encoder = LabelEncoder()
 encoder.fit(y)
 print(y[0])
 print(padded[0])
-lstm = CustomizedLSTM(vocab_size=5000)
+lstm = CustomizedLSTM(num_classes=1, num_hidden_layers=3, num_recurrent_units=[256,512,256], num_dense_layers=3, num_dense_neurons=[128,64,32], vocab_size=5000)
 lstm.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 lstm.fit(padded,y,validation_split=0.2, epochs=10)
 print(lstm.layers)
